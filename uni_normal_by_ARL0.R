a0dde6d1fe608c7f4a6396de1d772f3d249fdb56
Yuhui Yao <bolus123@hotmail.com>
2017-05-24 16:44:17 -0500

    new code for R&D

---
+++ uni_normal_by_ARL0.R
@@ -0,0 +1,67 @@
+#library(adehabitatLT)
+
+#indirect_method <- function(k, m, nu, subdivisions = 100){
+#
+#
+#    pcc.f <- function(Y, k, m, nu) {
+#    
+#        int <- 1 / (1 - pnorm(k / sqrt((m + 1) / m) * Y)) * dchi(Y, df = nu)
+#        int[is.infinite(int)] <- NaN
+#        int[is.nan(int)] <- 0
+#        
+#        int
+#    
+#    }
+#    
+#    #debug(pcc.f)
+#    
+#    integrate(pcc.f, lower = 0, upper = Inf, k = k, m = m, nu = nu, subdivisions = subdivisions)$value
+#
+#    
+#
+#}
+
+CC_norm <- function(
+                m, 
+                nu, 
+                ARL = 370, 
+                method = 'direct', 
+                alternative = '2-sided'
+){
+
+    if (method == 'direct'){
+    
+        L <- ifelse(alternative == '2-sided', qt(1 - 1 / ARL / 2, df = nu),qt(1 - 1 / ARL, df = nu))
+        k <- L / sqrt((m + 1) / m)
+    
+    }   else    {
+    
+        
+    
+    }
+    
+    k
+    
+
+}
+
+ARL_norm <- function(
+                k,
+                m, 
+                nu, 
+                delta = 0,
+                method = 'direct', 
+                alternative = '2-sided'
+){
+
+    if (method == 'direct'){
+    
+        L <- sqrt((m + 1) / m) * k - delta
+        p <- pt(L, df = nu)
+        ARL <- 1 / (1 - p)
+    
+    }
+
+    ARL
+
+}
